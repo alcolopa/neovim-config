1c49f2f145ee90ea46621044f2730d78de21843b
Emilio El Murr <84962687+alcolopa@users.noreply.github.com>
2024-11-19 07:46:53 +0200

    Added dev icons

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,12 +2,15 @@ return {
     {
         'nvim-telescope/telescope.nvim', 
         tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons'
+        },
         keys = {
            { "<leader>ff", ":Telescope find_files<CR>", desc = "Find files in telescope" },
-           { "<leader>fg", ":Telescope live_grep<CR>", desc = "Find files in telescope" },
-           { "<leader>fb", ":Telescope buffers<CR>", desc = "Find files in telescope" },
-           { "<leader>fh", ":Telescope help_tags<CR>", desc = "Find files in telescope" },
+           { "<leader>fg", ":Telescope live_grep<CR>", desc = "Telescipoe live grep" },
+           { "<leader>fb", ":Telescope buffers<CR>", desc = "Telescope buffers" },
+           { "<leader>fh", ":Telescope help_tags<CR>", desc = "Telescope help tags" },
         },
     }
 }
