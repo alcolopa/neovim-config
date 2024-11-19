2cb70ea59d6ca9d84e7e3c06c2291c4cf1b85983
Emilio El Murr <84962687+alcolopa@users.noreply.github.com>
2024-11-19 12:54:55 +0200

    adding autocomplete and lsp

---
+++ lua/plugins/autoclose.lua
@@ -0,0 +1,24 @@
+return {
+  -- Auto-close brackets, quotes, and parentheses
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true, -- Enable treesitter integration
+        fast_wrap = {},
+      })
+    end,
+  },
+
+  -- Auto-close and rename HTML/XML tags
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+}
+
---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,74 @@
+return {
+ {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",    -- LSP source for nvim-cmp
+      "hrsh7th/cmp-buffer",      -- Buffer source for nvim-cmp
+      "hrsh7th/cmp-path",        -- File path source for nvim-cmp
+      "hrsh7th/cmp-cmdline",     -- Command-line source for nvim-cmp
+      "L3MON4D3/LuaSnip",        -- Snippet engine
+      "saadparwaiz1/cmp_luasnip" -- Snippet source for nvim-cmp
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      -- nvim-cmp setup
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- Use buffer source for `/` and `?`
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Use path and cmdline source for `:`
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+}
