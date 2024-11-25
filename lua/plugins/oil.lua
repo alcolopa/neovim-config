dd9b7ccddf49d4d8bd63567e07efeef874ecc37a
Emilio El Murr <84962687+alcolopa@users.noreply.github.com>
2024-11-25 14:12:00 +0200

    adding neogit

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,6 @@
 return {
     {
         'stevearc/oil.nvim',
-        keys = {
-             {"-", "actions.parent", desc = "Open parents folder"}
-        },
         opts = {},
         dependencies = {
             {
