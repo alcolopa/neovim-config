19b40722641c8e40eabc6c57cda3982e4c709cc6
Emilio El Murr <84962687+alcolopa@users.noreply.github.com>
2024-11-26 17:13:57 +0200

    Updating a plugin and changing the keymap to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,28 @@
 return {
     {
-        'stevearc/oil.nvim',
-        opts = {},
-        dependencies = {
-            {
-                "echasnovski/mini.icons",
-                opts = {
-                }
+        "stevearc/oil.nvim",
+        lazy = false,
+        opts = {
+            columns = {
+                "icons",
+            },
+            skip_confirm_for_simple_edits = true,
+            prompt_save_on_select_new_entry = false,
+            default_file_explorer = true,
+            view_options = {
+                show_hidden = true,
+            },
+            keymaps = {
+                ["<leader>s"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+                ["<leader>h"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+                ["<leader>t"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in a second tab" },
             }
-        }
-    }
+        },
+        dependencies = {
+            "echasnovski/mini.icons",
+        },
+        config = function(_, opts)
+            require("oil").setup(opts)
+        end,
+    },
 }
